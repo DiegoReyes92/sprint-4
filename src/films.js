b0de716c99e581553985110e5c088c25e0fbe1bc
Diego <92diegoreyes@gmail.com>
2021-12-09 15:28:16 +0100

    mitad de proyecto

--- src/films.js
+++ src/films.js
@@ -1,23 +1,33 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
+  let result =  array.map(movie => movie.director);
   console.log("EXERCICE 1 ->", result);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+ let result = array.filter(movie => movie.director === director);
+ console.log("EXERCICE 2 ->", result);
+ return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  
+  let moviesDirector = array.filter ( movie => movie.director === director);
+  let result = moviesDirector.reduce((a, b) => a.score + b.score)/(moviesDirector.length).toFixed(2);
+  console.log("EXERCICE 3 ->", result);
+  return result;
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  
+  let orderTitle = array
+    .map(movie => movie.title)
+    .sort((a, b) => (a > b ? 1 : -1))
+    .slice(0, 20);
+  console.log('EXERCICE 4 ->', orderTitle);
+  return orderTitle;
 }
 
 // Exercise 5: Order by year, ascending
